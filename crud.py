9958780fc9de63f386d821efeefe3634697be09c
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-08-01 21:07:29 -0700

    Create movie function and rating completed

--- crud.py
+++ crud.py
@@ -9,6 +9,27 @@ def create_user(email, password):
 
     return user
 
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, 
+                  overview=overview, 
+                  release_date=release_date, 
+                  poster_path=poster_path)
+
+    return movie
+
+def create_rating(user, movie, score):
+    """Create and return new rating."""
+
+    rating = Rating(user=user,
+                    movie=movie,
+                    score=score)
+    
+    return rating
+
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
